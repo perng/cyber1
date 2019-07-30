5cd7a0d5bc4ade0ea7824c6816354f855009fbb2
Charles Perng <changshing.perng@wework.com>
2019-07-29 22:54:04 -0700

    completed solution

--- linux/exrd2/solution.sh
+++ linux/exrd2/solution.sh
@@ -1,22 +1,22 @@
-sudo nano /etc/apt/sources.list #check for malicious sources
-sudo nano /etc/resolv.conf #make sure if safe, use 8.8.8.8 for name server
-sudo nano /etc/hosts #make sure is not redirecting
-sudo nano /etc/rc.local #should be empty except for 'exit 0'
-sudo nano /etc/sysctl.conf #change net.ipv4.tcp_syncookies entry from 0 to 1
-sudo nano /etc/lightdm/lightdm.conf #allow_guest=false, remove autologin
-sudo nano /etc/ssh/sshd_config #Look for PermitRootLogin and set to no
+nano /etc/apt/sources.list #check for malicious sources
+nano /etc/resolv.conf #make sure if safe, use 8.8.8.8 for name server
+nano /etc/hosts #make sure is not redirecting
+nano /etc/rc.local #should be empty except for 'exit 0'
+nano /etc/sysctl.conf #change net.ipv4.tcp_syncookies entry from 0 to 1
+nano /etc/lightdm/lightdm.conf #allow_guest=false, remove autologin
+nano /etc/ssh/sshd_config #Look for PermitRootLogin and set to no
 
 #media file deletion
-sudo find / -name '*.mp3' -type f -delete
-sudo find / -name '*.mov' -type f -delete
-sudo find / -name '*.mp4' -type f -delete
-sudo find / -name '*.avi' -type f -delete
-sudo find / -name '*.mpg' -type f -delete
-sudo find / -name '*.mpeg' -type f -delete
-sudo find / -name '*.flac' -type f -delete
-sudo find / -name '*.m4a' -type f -delete
-sudo find / -name '*.flv' -type f -delete
-sudo find / -name '*.ogg' -type f -delete
+find / -name '*.mp3' -type f -delete
+find / -name '*.mov' -type f -delete
+find / -name '*.mp4' -type f -delete
+find / -name '*.avi' -type f -delete
+find / -name '*.mpg' -type f -delete
+find / -name '*.mpeg' -type f -delete
+find / -name '*.flac' -type f -delete
+find / -name '*.m4a' -type f -delete
+find / -name '*.flv' -type f -delete
+find / -name '*.ogg' -type f -delete
 
 # remove image files (seems no need for this round)
 #sudo find /home -name '*.gif' -type f -delete
@@ -25,62 +25,74 @@ sudo find / -name '*.ogg' -type f -delete
 #sudo find /home -name '*.jpeg' -type f -delete
 
 # firewall
-sudo ufw enable
-sudo ufw deny 23
-sudo ufw deny 2049
-sudo ufw deny 515
-sudo ufw deny 111
+ufw enable
+ufw deny 23
+ufw deny 2049
+ufw deny 515
+ufw deny 111
+
+# password age, set the following 2 lines
+# PASS_MAX_DAYS   30
+# PASS_MIN_DAYS   1
+nano /etc/login.defs
 
 #find all admin users
 grep -Po '^sudo.+:\K.*$' /etc/group
 # output: ballen,iwest,hwells,jgarrick,jwells,wwest,jwest
 # wwest and jwest are not supposed to be admin
-sudo deluser wwest sudo
-sudo deluser wwest adm  # wwest was never in adm
-sudo deluser jwest sudo
+deluser wwest sudo
+deluser wwest adm  # wwest was never in adm
+deluser jwest sudo
 
 # find all users
 grep -v false /etc/passwd | grep -v nologin | cut -d: -f1
 
 # disable root login
-sudo chsh -s /bin/sbin/nologin root
+chsh -s /bin/sbin/nologin root
 
 # delete un-authorized users
 # TODO: write a function to compare authorized users
-sudo  userdel -r brainiac
-sudo  userdel -r alchemy 
+ userdel -r brainiac
+ userdel -r alchemy 
 
 # change password, but not for ballen
 echo -e "iwest: JiTTerS\nhwells: thawne25\njgarrick: ZoLomOn\njwells: quick" |sudo chpasswd 
 
-sudo apt-get remove minetest
+apt-get remove minetest
 grep minetest ~/Desktop/*
 # you should see "/home/ballen/Desktop/minetest.desktop" contains minetest
 rm /home/ballen/Desktop/minetest.desktop
 
-sudo apt-get remove ophcrack 
+apt-get remove ophcrack 
 grep  ophcrack ~/Desktop/*
 rm /home/ballen/Desktop/ophcrack.desktop
 
+# the last bit with no command line solution yet
+# open "software updater", "Settings...", "Updates" 
+# check "trusty-security" and "trusty-updates"
+# select "Automatically check for updates" to Daily
+
 
 # update software
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get -y autoremove
-sudo apt-get upgrade firefox
+apt-get update
+apt-get upgrade
+apt-get -y autoremove
+apt-get upgrade firefox
 
+# need a reboot 
 
 #install openssh
-sudo apt-get install -y openssh-server
+apt-get install -y openssh-server
 
 # remove SMB (samba) server
-sudo apt-get remove samba
+apt-get remove samba
 grep samba /etc/group
 # sambashare should be removed
-sudo groupdel sambashare 
+groupdel sambashare 
 
 
-sudo ufw enable
-
-sudo nano /etc/ssh/sshd_config 
+nano /etc/ssh/sshd_config 
 # change "#PermitRootLogin yes" to "#PermitRootLogin no"
+
+
+
