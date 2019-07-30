4b6aba7258ccb3bbc0d909355e297b3f50dc6393
Charles Perng <changshing.perng@wework.com>
2019-07-29 22:08:26 -0700

    add a few commands

--- linux/exrd2/solution.sh
+++ linux/exrd2/solution.sh
@@ -1,3 +1,36 @@
+sudo nano /etc/apt/sources.list #check for malicious sources
+sudo nano /etc/resolv.conf #make sure if safe, use 8.8.8.8 for name server
+sudo nano /etc/hosts #make sure is not redirecting
+sudo nano /etc/rc.local #should be empty except for 'exit 0'
+sudo nano /etc/sysctl.conf #change net.ipv4.tcp_syncookies entry from 0 to 1
+sudo nano /etc/lightdm/lightdm.conf #allow_guest=false, remove autologin
+sudo nano /etc/ssh/sshd_config #Look for PermitRootLogin and set to no
+
+#media file deletion
+sudo find / -name '*.mp3' -type f -delete
+sudo find / -name '*.mov' -type f -delete
+sudo find / -name '*.mp4' -type f -delete
+sudo find / -name '*.avi' -type f -delete
+sudo find / -name '*.mpg' -type f -delete
+sudo find / -name '*.mpeg' -type f -delete
+sudo find / -name '*.flac' -type f -delete
+sudo find / -name '*.m4a' -type f -delete
+sudo find / -name '*.flv' -type f -delete
+sudo find / -name '*.ogg' -type f -delete
+
+# remove image files (seems no need for this round)
+#sudo find /home -name '*.gif' -type f -delete
+#sudo find /home -name '*.png' -type f -delete
+#sudo find /home -name '*.jpg' -type f -delete
+#sudo find /home -name '*.jpeg' -type f -delete
+
+# firewall
+sudo ufw enable
+sudo ufw deny 23
+sudo ufw deny 2049
+sudo ufw deny 515
+sudo ufw deny 111
+
 #find all admin users
 grep -Po '^sudo.+:\K.*$' /etc/group
 # output: ballen,iwest,hwells,jgarrick,jwells,wwest,jwest
